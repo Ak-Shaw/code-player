ca8a1a1e8e7a75b77d64c262cb871d2318c1a7f4
Ak-Shaw <ayushshawz@gmail.com>
2020-08-15 03:50:23 +0530

    code-player first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function updateOutput(){
+    
+    $("iframe").contents().find("html").html(
+"<html><head><style type='text/css'>"+$("#cssPanel").val()+"</style></head><body>"+$("#htmlPanel").val()+"</body></html>");
+    
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+    
+}
+
+$(".toggleButton").hover(function(){
+    
+    $(this).addClass("highlightedButton");
+}, function(){
+    
+    $(this).removeClass("highlightedButton");
+})
+
+$(".toggleButton").click(function(){
+    
+    $(this).toggleClass("active");
+    
+    $(this).removeClass("highlightedButton");
+    
+    var panelId=$(this).attr("id")+"Panel";
+    
+    $("#"+panelId).toggleClass("hidden");
+    
+    var numberOfActivePanels=4-$('.hidden').length;
+    
+    $(".panel").width($(window).width()/numberOfActivePanels-10);
+    
+    if(numberOfActivePanels==0){
+        
+        $("#allInactive").css("display","block");
+    } else{
+        
+        $("#allInactive").css("display","none");
+    }
+})
+
+$(".panel").height($(window).height()-$("#header").height());
+
+$(".panel").width($(window).width()/2-10);
+
+updateOutput();
+
+$("textarea").on('change keyup paste',function(){
+   
+updateOutput();
+
+
+});
